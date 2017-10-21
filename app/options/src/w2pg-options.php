37a7b298f7c94b7fef7b21093d3041c8add20cbd
Leandro de Amorim <androrim@gmail.com>
2017-10-21 16:08:00 -0200

    Default options

--- app/options/src/w2pg-options.php
+++ app/options/src/w2pg-options.php
@@ -13,19 +13,56 @@ add_action('admin_menu', 'w2pg_options_admin_menu');
 
 function w2pg_options()
 {
-    $not_in = unserialize(W2PGNOTIN);
-    $options = get_option('w2pg_optionse');
+    $not_in  = unserialize(W2PG_NOTIN);
+    $options = w2pg_get_options();
+
+    if ($options === false) {
+        $options = unserialize(W2PG_OPTDEFAULT);
+    }
 
     require W2PG_OPTIONSDIR . '/templates/w2pg-options-tpl.php';
 }
 
 function w2pg_options_save()
 {
-    if (!wp_verify_nonce($_POST['w2pg_noncename'], W2PGNONCE)) {
+    if (!wp_verify_nonce($_POST['w2pg_noncename'], W2PG_NONCE)) {
         wp_die('<h1>WP Posts Galleries</h1> <p>' . __('Invalid request') . '</p>');
     }
 
-    if (isset($_POST['w2pg_options'])) {
-        update_option('w2pg_options', $_POST['w2pg_options']);
+    $options = isset($_POST['w2pg_options']) ? $_POST['w2pg_options'] : null;
+
+    update_option(W2PG_OPTNAME, $options);
+}
+
+function w2pg_get_options($name = '')
+{
+    $options = get_option(W2PG_OPTNAME);
+    $default = unserialize(W2PG_OPTDEFAULT);
+
+    var_dump($options);
+
+    if ($options === false) {
+        return $default;
     }
+
+    if ($options === '') {
+        foreach ($default as $i => $v) {
+            if ($i == 'in') {
+                $default[$i] = array();
+            }
+            else {
+                $default[$i] = '';
+            }
+        }
+
+        $options = $default;
+    }
+
+    if (isset($options[$name])) {
+        return $options[$name];
+    }
+
+    $GLOBALS['w2pg_options'] = $options;
+
+    return $options;
 }
--- config.php
+++ config.php
@@ -4,20 +4,25 @@
  * The base configuration for WP Post Galleries
  */
 
-define('W2PGNONCE', 'androrim_w2pg');
-
 define('W2PGDIR', dirname(__FILE__));
+
+define('W2PG_NONCE', 'androrim_w2pg');
+
+define('W2PG_OPTNAME', 'w2pg_options');
 
 define('W2PG_OPTIONSDIR', W2PGDIR . '/app/options');
 
-define('W2PGNOTIN',
-    serialize(array(
+define('W2PG_NOTIN', serialize(array(
     'attachment',
     'revision',
     'nav_menu_item',
     'custom_css',
     'customize_changeset',
     'oembed_cache',
+)));
+
+define('W2PG_OPTDEFAULT', serialize(array(
+    'in' => array('post')
 )));
 
 
