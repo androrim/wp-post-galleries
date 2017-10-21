c75d224e49cc6f73d9cee26811f649b338594138
Leandro de Amorim <androrim@gmail.com>
2017-10-21 10:23:50 -0200

    Header plugin

---
+++ wp-post-galleries.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: WP Post Galleries
+Plugin URI: https://androrim.github.io/wp-post-galleries/
+Description: Metabox galleries to WordPress post
+Version: 1.0
+Author: Leandro de Amorim <androrim@gmail.com>
+Author URI: https://www.linkedin.com/in/leandrodeamorim
+License: GPLv2
+*/
