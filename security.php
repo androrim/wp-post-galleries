8b205a70d449f973bf2a1baf17324738aa9c7a71
Leandro de Amorim <androrim@gmail.com>
2017-10-23 20:41:14 -0200

    Sanitize escape js from data key to save

--- security.php
+++ security.php
@@ -10,7 +10,9 @@ function w2pg_sanitize_request_data($data)
         $sanitized = [];
 
         foreach ($data as $key => $value) {
-            $sanitized[esc_sql($key)] = w2pg_sanitize_request_data($value);
+            $key = esc_sql($key);
+            $key = esc_js($key);
+            $sanitized[$key] = w2pg_sanitize_request_data($value);
         }
     }
 
