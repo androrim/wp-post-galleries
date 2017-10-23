229006e2f4f5a82c6d03b59ae78ccc97829335e4
Leandro de Amorim <androrim@gmail.com>
2017-10-22 23:39:09 -0200

    Options default gallery base template item

--- app/options/templates/w2pg-template-item-options-tpl.php
+++ app/options/templates/w2pg-template-item-options-tpl.php
@@ -6,13 +6,23 @@
     </th>
     <td>
         <textarea name="w2pg_options[template]"
-                  id="w2pg-gallery-template-item" rows="8" cols="60"><?=
-                      isset($options['template']) ? str_replace('\\', '',
-                              $options['template']) : null
+                  id="w2pg-gallery-template-item" rows="8" cols="60"><?php
+                      if (isset($options['template'])) {
+                          echo str_replace('\\', '', $options['template']);
+                      }
+                      else {
+                          echo '<a id="w2pg-%ID%" class="w2pg-item" title="%LEGEND%" href="%IMG%">'
+                          . '<img  src="%IMG_MEDIUM%" alt="%ALT%" />'
+                          . '<span class="title">%TITLE%</spam>'
+                          . '<span class="description">%DESCRIPTION%</spam>'
+                          . '<a>';
+                      }
                       ?></textarea>
 
-        <p class="description"><?php _e('This field is base template for output '
-            . 'html items when use <strong>wp_post_gallery</strong> function.') ?></p>
+        <p class="description"><?php
+            _e('This field is base template for output '
+                . 'html items when use <strong>wp_post_gallery</strong> function.')
+            ?></p>
 
         <h3><?php _e('Valid template codes') ?></h3>
 
@@ -29,8 +39,8 @@
         <p>
             <strong><?php _e('Size') ?>:</strong> 
             <?php _e('Full') ?>: <code>%IMG%</code> |
-            <?php _e('Medium') ?>: <code>%IMG_MEDIUM%</code> |
-            <?PHP _e('Tumbnail') ?>: <code>%IMG_THUMB%</code>
+<?php _e('Medium') ?>: <code>%IMG_MEDIUM%</code> |
+<?PHP _e('Tumbnail') ?>: <code>%IMG_THUMB%</code>
         </p>
 
 
