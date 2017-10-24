8b4eb6921b8aba7486e712b810f48a0ffe6aa980
Leandro de Amorim <androrim@gmail.com>
2017-10-24 21:51:18 -0200

    Created updated options message

--- app/options/src/w2pg-options.php
+++ app/options/src/w2pg-options.php
@@ -2,8 +2,7 @@
 
 function w2pg_options_admin_menu()
 {
-    add_options_page(__('WP Post Gallery'), __('WP Post Gallery'), 
-            'manage_options', 'wp-post-gallery', 'w2pg_options');
+    add_options_page(__('WP Post Gallery'), __('WP Post Gallery'), 'manage_options', 'wp-post-gallery', 'w2pg_options');
 }
 
 add_action('admin_menu', 'w2pg_options_admin_menu');
@@ -13,7 +12,7 @@ function w2pg_options()
     if (isset($_POST['w2pg_options_save']) && wp_verify_nonce($_POST['w2pg_noncename'], W2PG_NONCE)) {
         w2pg_options_save($_POST);
     }
-    
+
     $not_in = unserialize(W2PG_NOTIN);
     $options = w2pg_get_options();
 
@@ -29,12 +28,23 @@ function w2pg_options_save($data)
     if (!wp_verify_nonce($_POST['w2pg_noncename'], W2PG_NONCE)) {
         wp_die('<h1>WP Posts Galleries</h1> <p>' . __('Invalid request') . '</p>');
     }
-    
+
     $options = null;
-      
+
     if (isset($data['w2pg_options']['in'])) {
-       $options = w2pg_sanitize_request_data($data['w2pg_options']);
+        $options = w2pg_sanitize_request_data($data['w2pg_options']);
     }
-    
-    update_option(W2PG_OPTNAME, $options);
+
+    if (update_option(W2PG_OPTNAME, $options)){
+        w2pg_update_notice(__('Options saved with success!'), 'success');
+    }
+}
+
+function w2pg_update_notice($message, $type)
+{
+    ?>
+    <div class="notice notice-<?= $type ?> is-dismissible">
+        <p><?= $message ?></p>
+    </div>
+    <?php
 }
